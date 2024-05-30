26d9b3e531d0e5367f324b430310e466cf6103f3
Mari-Anne Mee <mari-anne@MariAnnesLaptop.lan>
2024-05-30 14:53:49 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemLinq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week16BitCoinApp
+{
+    public class BitCoinRate
+    {
+        public bpi bpi { get; set; }
+    }
+
+
+
+    public class bpi
+    {
+        public EUR EUR { get; set; }
+    }
+
+    public class EUR
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+
+}
+
